ade27e273796c9cb8e0b086aac2f086bf9826d84
Marcella <116006863+Marcellafr@users.noreply.github.com>
2023-01-14 20:57:21 +0700

    Add files via upload

---
+++ Database/pelanggan_ds.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `pelanggan_ds`(
+	`id_customer` char(9),
+    `level` char(7),
+    `nama` varchar(20),
+    `id_cabang_sales` char(5),
+    `cabang_sales` varchar(10),
+    `id_distributor` varchar(3),
+    `group` varchar(6)
+);
